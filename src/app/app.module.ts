6931a22e430bdcd2c57f857371b9167ef3aef3d2
Avi <avi@picangelo.com>
2021-01-29 11:12:54 +0200

    My first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HoverDirective } from './directives/hover.directive';
+import { RoundCornersDirective } from './directives/round-corners.directive';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { GenericButtonComponent } from './components/generic-button/default-button.component';
+import { FieldHeaderComponent } from './components/field-header/field-header.component';
+import { CustomTextareaComponent } from './components/custom-textarea/custom-textarea.component';
 
+// test
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenericButtonComponent,
+    HoverDirective,
+    RoundCornersDirective,
+    FieldHeaderComponent,
+    CustomTextareaComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTooltipModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
